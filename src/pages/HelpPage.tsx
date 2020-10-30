022274a66b8ce665a0e4d52469740edcdbc3b61e
Tyler Matzelle <tmatzelle16560@alumni.focuscollege.com>
2020-10-29 19:32:53 -0700

    things happened

--- src/pages/HelpPage.tsx
+++ src/pages/HelpPage.tsx
@@ -1,21 +1,10 @@
 import React from 'react'
-<<<<<<< HEAD
-import MainLayout from '../layouts/MainLayout';
-=======
 import MainLayout from '../layouts/MainLayout'
->>>>>>> 35cbc66e9e61a00da6a8615fd1a5df631441d9c1
 
 export default function HelpPage( props:{} ){
 
     return <MainLayout>
-<<<<<<< HEAD
-        <h1>
-            Help Page
-        </h1>
-    </MainLayout>;
-=======
         <h1>Help Page</h1>
     </MainLayout>;
 
->>>>>>> 35cbc66e9e61a00da6a8615fd1a5df631441d9c1
 }
--- src/pages/checkout/CheckoutSuccessPage.tsx
+++ src/pages/checkout/CheckoutSuccessPage.tsx
@@ -3,12 +3,8 @@ import MainLayout from '../../layouts/MainLayout';
 
 export default function CheckoutSuccessPage( props:{} ){
 
-<<<<<<< HEAD
-    return <h1>Checkout Success</h1>;
-=======
     return <>
         <h1>Checkout Success</h1>
     </>;
 
->>>>>>> 35cbc66e9e61a00da6a8615fd1a5df631441d9c1
 }
