57b2e237a70ae82d8baa4e34062798a226f1e99e
Doug Waffle <dwaffle13154@alumni.focuscollege.com>
2020-10-31 13:38:55 -0700

    Prepull commit

---
+++ src/pages/cart/CheckoutPage.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Route, Switch } from 'react-router';
+import CheckoutSuccess from '../checkout/CheckoutSuccessPage';
+import CheckoutFailure from '../checkout/CheckoutFailedPage';
+import { Link } from 'react-router-dom';
+import MainLayout from '../../layouts/MainLayout';
+
+export default function CheckoutPage( props:{} ){
+
+    return <MainLayout>
+        <Switch>
+            <Route path="/cart/checkout/success" component={CheckoutSuccess}/>
+            <Route path="/cart/checkout/failure" component={CheckoutFailure}/>
+            <Route path="/cart/checkout">
+
+                <h1>Checkout Page</h1>
+                <Link to="/cart/checkout/success">Success</Link>
+                <Link to="/cart/checkout/failure">Failed</Link>
+
+            </Route>
+        </Switch>
+
+
+    </MainLayout>;
+
+}
