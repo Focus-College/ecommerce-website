e376e42af37e89866b3bb62a10934d9a12e03d7e
Doug Waffle <dwaffle13154@alumni.focuscollege.com>
2020-11-01 10:43:33 -0800

    quack

--- src/components/ProductList/Product.tsx
+++ src/components/ProductList/Product.tsx
@@ -2,19 +2,25 @@ import { getValueFromDenormalizedStringPath } from '../../helpers/getValueFromDe
 import Images from '../../images';
 
 import React, { useEffect, useState } from 'react';
-import { Badge, Card } from 'react-bootstrap';
+import { Badge, Button, Card } from 'react-bootstrap';
 import { IProduct } from './HorizontalProductList';
 import './style.scss';
 
 import Ribbon from '../CornerRibbon/CornerRibbon';
+import { useShoppingCart } from '../../pages/cart/useShoppingCart';
 
 export default function Product( props:IProduct ){
 
+    const { isInCart, addToCart } = useShoppingCart();
     const [ showClearance, setShowClearance ] = useState( false );
 
     useEffect(() => {
         setShowClearance( props.price.substr(-2,2) === "45" );
-    }, [])
+    }, []);
+
+    const clickAddToCartHandler = () => {
+        addToCart( props );
+    }
 
     return (
         <Card className="product-list-product ribbon-ctr">
@@ -26,6 +32,8 @@ export default function Product( props:IProduct ){
                 <Card.Text className="product-description">
                     {props.description}
                 </Card.Text>
+                {isInCart(props) || <Button block onClick={clickAddToCartHandler}>Add to Cart</Button>}
+                {isInCart(props) && <Button block disabled variant="success">Added to Cart!</Button>}
             </Card.Body>
             <Card.Footer>
                 <div>
