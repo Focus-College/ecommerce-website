aa671cd5486f484027ef84d57678b58478974a60
Colby <cboettcher16590@alumni.focuscollege.com>
2020-11-09 22:09:29 -0800

    Created new Reviews and Q&A Tab component, and separate stylesheet. Integrated component into projects page

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -6,6 +6,7 @@ import HorizontalProductList from '../components/ProductList/HorizontalProductLi
 import Images from '../images';
 import { getValueFromDenormalizedStringPath } from '../helpers/getValueFromDenormalizedStringPath';
 import { Col, Container, Row, Tab, Tabs } from 'react-bootstrap';
+import Reviews_QandA_Tab from '../components/Reviews_QandA/Reviews_QandA_Tab';
 
 export default function ProductPage( props:IProduct ){
 
@@ -21,37 +22,7 @@ export default function ProductPage( props:IProduct ){
             </div>
         </section>
 
-        <div className="questions-and-reviews">
-
-        <Tabs defaultActiveKey="reviews" id="review-qa-tab">
-            <Tab eventKey="reviews" title="Reviews">
-                <section className="reviews-Section">
-
-                </section>
-            </Tab>
-            <Tab eventKey="QaA" title="Questions and Answers">
-                <section>
-                <Container className="qAnda-container">
-                    <Row >
-                        <Col className="title-text" xs={2}>Question: </Col>
-                        <Col className="qAnda-text">Is it really useful for sunburn?</Col>
-                    </Row>
-                    <hr/>
-                    <Row>
-                        <Col className="title-text" xs={2}>Answer: </Col>
-                        <Col className="qAnda-text">Yes, it is useful for minor sunburns such as heat and 
-                        redness of the skin. It can soothe the skin. If the sunburn 
-                        is severe and peeling, it may not be suitable. Hope it helps you.
-                        </Col>
-                    </Row>
-
-                </Container>
-                
-                </section>
-            </Tab>
-        </Tabs>
-
-        </div>
+    <Reviews_QandA_Tab></Reviews_QandA_Tab>
 
         <section>
         <h2>Suggested Products</h2>
