1b6cba9df4bd68b83f974a3ccb09e71f8750f816
Colby <cboettcher16590@alumni.focuscollege.com>
2020-11-09 19:23:35 -0800

    Added Questions And Answers & Reviews Tab to Product Page. Filled out sample data in Q&A Section

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -5,6 +5,7 @@ import { IProduct } from '../components/ProductList/HorizontalProductList';
 import HorizontalProductList from '../components/ProductList/HorizontalProductList';
 import Images from '../images';
 import { getValueFromDenormalizedStringPath } from '../helpers/getValueFromDenormalizedStringPath';
+import { Col, Container, Row, Tab, Tabs } from 'react-bootstrap';
 
 export default function ProductPage( props:IProduct ){
 
@@ -19,11 +20,38 @@ export default function ProductPage( props:IProduct ){
 
             </div>
         </section>
-        <section>
 
-            {/* Reviews / Question & Answers */}
+        <div className="questions-and-reviews">
 
-        </section>
+        <Tabs defaultActiveKey="reviews" id="review-qa-tab">
+            <Tab eventKey="reviews" title="Reviews">
+                <section className="reviews-Section">
+
+                </section>
+            </Tab>
+            <Tab eventKey="QaA" title="Questions and Answers">
+                <section>
+                <Container className="qAnda-container">
+                    <Row >
+                        <Col className="title-text" xs={2}>Question: </Col>
+                        <Col className="qAnda-text">Is it really useful for sunburn?</Col>
+                    </Row>
+                    <hr/>
+                    <Row>
+                        <Col className="title-text" xs={2}>Answer: </Col>
+                        <Col className="qAnda-text">Yes, it is useful for minor sunburns such as heat and 
+                        redness of the skin. It can soothe the skin. If the sunburn 
+                        is severe and peeling, it may not be suitable. Hope it helps you.
+                        </Col>
+                    </Row>
+
+                </Container>
+                
+                </section>
+            </Tab>
+        </Tabs>
+
+        </div>
 
         <section>
         <h2>Suggested Products</h2>
